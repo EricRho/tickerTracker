b14b7772d22d8e263512fe78ff9d1b852dc65999
EricRho <eric.rho.87@gmail.com>
2015-05-11 11:53:33 -0400

    Can now add/create a stock to follow

--- app/assets/javascripts/angular/stocksCtrl.js
+++ app/assets/javascripts/angular/stocksCtrl.js
@@ -1,8 +1,15 @@
-app.controller('stocksCtrl', ['$scope', 'Stock', function($scope, Stock) {
+app.controller('stocksCtrl', ['$scope', 'Stock', '$filter', function($scope, Stock, $filter) {
   $scope.stocks = Stock.all();
 
   $scope.deleteStock = function(id, idx) {
     $scope.stocks.splice(idx, 1);
     return Stock.delete(id);
-  }
+  };
+  $scope.createStock = function() {
+    var attr = {};
+    attr.symbol = $filter('uppercase')($scope.newCompany);
+    var newStock = Stock.create(attr);
+    $scope.stocks.push(newStock);
+    $scope.newCompany = '';
+  };
 }]);
--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -8,4 +8,15 @@ class StocksController < ApplicationController
   def destroy
     respond_with Stock.destroy(params[:id])
   end
+
+  def create
+    respond_with Stock.create(stock_params)
+  end
+
+  private
+
+  def stock_params
+    params.require(:stock).permit(:symbol, :name, :ask, :bid, :change, :change_in_percent)
+  end
+
 end
