db30d045a4c5d2e0dc64b35061476e59d5e60386
EricRho <eric.rho.87@gmail.com>
2015-07-23 16:34:41 -0400

    Import CSV file of Stock Symbols for Derivative search

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,5 @@
 Stock.delete_all
 
-# Stock.create!(symbol: 'MNKD', name: 'Mannkind Corporation')
-# Stock.create!(symbol: 'NVDA', name: 'NVIDIA Corporation')
-# Stock.create!(symbol: 'AMZN', name: 'Amazon.com Inc.')
-# Stock.create!(symbol: 'AMD', name: 'Advanced Micro Devices Inc.')
-# Stock.create!(symbol: 'INTC', name: 'Intel Corporation')
-
 # Derivative.delete_all
 
 # response = HTTParty.get('http://query.yahooapis.com/v1/public/yql?q=select%20%2A%20from%20yahoo.finance.industry%20where%20id%20in%20(select%20industry.id%20from%20yahoo.finance.sectors)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
@@ -17,13 +11,23 @@ Stock.delete_all
 #   end
 # end
 
+# Derivative.delete_all
+
+# Derivative.create!(symbol: 'AAPL', name: 'Apple')
+# Derivative.create!(symbol: 'MSFT', name: 'Microsoft')
+# Derivative.create!(symbol: 'GOOG', name: 'Google')
+# Derivative.create!(symbol: "BARC.L", name: 'Barclays')
+# Derivative.create!(symbol: 'TWTR', name: 'Twitter')
+# Derivative.create!(symbol: 'FB', name: 'Facebook')
+# Derivative.create!(symbol: "SIE.DE", name: "Siemens")
+# Derivative.create!(symbol: 'MNKD', name: 'MannKind Corporation')
+
 Derivative.delete_all
 
-Derivative.create!(symbol: 'AAPL', name: 'Apple')
-Derivative.create!(symbol: 'MSFT', name: 'Microsoft')
-Derivative.create!(symbol: 'GOOG', name: 'Google')
-Derivative.create!(symbol: "BARC.L", name: 'Barclays')
-Derivative.create!(symbol: 'TWTR', name: 'Twitter')
-Derivative.create!(symbol: 'FB', name: 'Facebook')
-Derivative.create!(symbol: "SIE.DE", name: "Siemens")
-Derivative.create!(symbol: 'MNKD', name: 'MannKind Corporation')
+require 'csv'
+
+csv_text = File.read('companylist.csv')
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+  Derivative.create!(row.to_hash)
+end
