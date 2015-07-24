256eb40aba59431867a388b0584a619c93642d22
EricRho <eric.rho.87@gmail.com>
2015-07-24 16:38:21 -0400

    Add auto-updating clock

---
+++ app/assets/javascripts/angular/clockCtrl.js
@@ -0,0 +1,9 @@
+app.controller('clockCtrl', ['$scope', function($scope) {
+  $scope.clock = new Date();
+  var updateClock = function() {
+    $scope.clock = new Date();
+  };
+  setInterval(function() {
+    $scope.$apply(updateClock);
+  }, 1000);
+}]);
