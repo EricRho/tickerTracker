50ed65bf70135c8a8a6f328fdcc83017fa87832c
EricRho <eric.rho.87@gmail.com>
2015-07-24 17:09:21 -0400

    Create Tabs and Corresponding Tabs controller in Angular

---
+++ app/assets/javascripts/angular/controllers/tabsCtrl.js
@@ -0,0 +1,22 @@
+app.controller('tabsCtrl', ['$scope', function($scope) {
+  $scope.tabs = [{
+    title: 'Profile',
+    url: 'one.tpl.html'
+  }, {
+    title: 'My Stocks',
+    url: 'two.tpl.html'
+  }, {
+    title: 'News Feed',
+    url: 'three.tpl.html'
+  }];
+
+  $scope.currentTab = 'one.tpl.html';
+
+  $scope.onClickTab = function(tab) {
+    $scope.currentTab = tab.url;
+  };
+
+  $scope.isActiveTab = function(tabUrl) {
+    return tabUrl == $scope.currentTab
+  };
+}]);
