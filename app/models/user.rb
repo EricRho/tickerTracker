f2bc69f28d39e48baf5ed0c1729cce16d53d759d
EricRho <eric.rho.87@gmail.com>
2015-07-28 15:42:51 -0400

    Can login using Twitter or own email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
   has_many :authorizations, :dependent => :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'welcome/index'
+
   devise_for :users,
   :controllers => {
     :omniauth_callbacks => 'users/omniauth_callbacks'
   }
+
+  # devise_scope :user do
+  #   get '/api/current_user' => 'users/sessions#show_current_user'
+  #   post '/api/check/is_user' => 'users/users#is_user', as: 'is_user'
+  # end
 
   scope :api do
     resources :stocks, defaults: {format: :json} do
@@ -11,5 +18,14 @@ Rails.application.routes.draw do
     end
     get :derivatives, to: 'derivatives#index'
   end
-  root 'my_watch_list#index'
+
+  unauthenticated :user do
+    root :to => 'welcome#index'
+  end
+
+  authenticated :user do
+    devise_scope :user do
+      get '/' => 'my_watch_list#index'
+    end
+  end
 end
