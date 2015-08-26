7ccebad09b934895838f40b7541850f96d5f957e
EricRho <eric.rho.87@gmail.com>
2015-08-26 13:56:58 -0400

    Add simple JQuery fun to homescreen

--- app/assets/javascripts/jquery/effects.js
+++ app/assets/javascripts/jquery/effects.js
@@ -1,5 +1,7 @@
 Effects = {
   doThem: function() {
+    $('#background').hide();
     $('#background').fadeIn(900);
+    $('img').slideUp(300).delay(350).fadeIn(800);
   }
 };
--- app/assets/javascripts/jquery/mainJQ.js
+++ app/assets/javascripts/jquery/mainJQ.js
@@ -1,3 +1,7 @@
-$(document).ready(function() {
+// $(document).ready(function() {
+//   Effects.doThem();
+// });
+
+$(document).on('ready page:change', function() {
   Effects.doThem();
 });
